c3955edf46d9830f5140b89e2ea06e7771739452
hallisonx <38287389+hallisonx@users.noreply.github.com>
2019-08-23 15:07:17 -0300

    Update gravar_placas_mongo.py

--- gravar_placas_mongo.py
+++ gravar_placas_mongo.py
@@ -1,24 +1,29 @@
 import pymongo
 from ast import literal_eval
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client['placas']
-coll = db['veiculos']
 
-#gravarNoBanco(coll)
-
-placa = coll.find_one({'Placa': 'OGA3304'})
-print(type(placa))
-print(placa['Marca/Modelo'])
-print(client.list_database_names())
-
-
-
-def gravarNoBanco(coll):
-	arq = open('OGE_0001_9999.txt', 'r')
+def salvarColecao(coll):
+	arq = open('QSA_0001_5999.txt', 'r')
 	texto = arq.readlines()
 	for linha in texto :
 	    print(linha)
 	    python_dict = literal_eval(linha)
 	    result = coll.insert_one(python_dict)
 	arq.close()
+
+
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client['placas']
+coll = db['veiculos']
+
+#salvarColecao(coll)
+
+placas = coll.find({'Marca/Modelo': 'FORD/KA SE 1.5'})
+for cursor in placas:
+    #print(cursor)
+	print(cursor['Ano Modelo'])
+
+print(client.list_database_names())
+
+# FONTE: https://realpython.com/introduction-to-mongodb-and-python/
